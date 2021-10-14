fc17e687556d553eab9b749c717997a72132cc12
Gerd-Lothar Reschke <gl@reschke.de>
2021-10-14 16:30:19 +0200

    translation update

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Gerd-Lothar Reschke <gl@reschke.de>
+ */
+$lang['name']                  = 'Einstellung für Seitenweiterleitungen';
+$lang['saved']                 = 'Daten gespeichert.';
+$lang['redirected']            = 'Sie wurden von <b>%s</b> weitergeleitet.';
---
+++ lang/de-informal/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Gerd-Lothar Reschke <gl@reschke.de>
+ */
+$lang['showmsg']               = 'Nutzer bei Weiterleitungen informieren';
