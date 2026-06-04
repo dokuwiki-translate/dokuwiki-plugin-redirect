32f069a55b63c34f55aa70e4dfb1a81a7e435f68
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:56:15 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['name']                  = 'Nastavenie presmerovaní stránok';
+$lang['saved']                 = 'Dáta uložené.';
+$lang['redirected']            = 'Boli ste presmerovaní z <b>%s</b>.';
